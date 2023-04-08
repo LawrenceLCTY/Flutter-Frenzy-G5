24a6ef9475db3d910999d5b87c31f8d12daa608b
LawrenceLCTY <lawrencechiengtzeyao@gmail.com>
2023-04-08 20:42:04 +0800

    init

---
+++ dart_application_g5/bin/dart_application_g5.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_g5/dart_application_g5.dart' as dart_application_g5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_g5.calculate()}!');
+}
---
+++ dart_application_g5/test/dart_application_g5_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_g5/dart_application_g5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
